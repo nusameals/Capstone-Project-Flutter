2dc09372133584620860fd62856e5038ebf28a5d
fda <fauziabusiness@gmail.com>
2023-06-05 20:26:21 +0700

    update notification

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
 
         primarySwatch: Colors.blue,
+        useMaterial3: true,
       ),
       home: Notifications(),
     );
--- lib/view/home/notification_screen.dart
+++ lib/view/home/notification_screen.dart
@@ -13,7 +13,7 @@ class _NotificationsState extends State<Notifications> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Notifications'),
+          title: const Text('Notifications', style: TextStyle(fontFamily: 'Poppins'),),
           leading: IconButton(
               onPressed: (){},
               icon: Icon(Icons.arrow_back)),
@@ -30,7 +30,8 @@ class _NotificationsState extends State<Notifications> {
                 TextButton(
                     onPressed: (){
 
-                    }, child: Text('Clear All')
+                    }, child: Text('Clear All',
+                  style: TextStyle(color: Colors.blue,fontSize: 15,fontFamily: 'Poppins'),)
                 ),
               ],
             ),
@@ -39,14 +40,16 @@ class _NotificationsState extends State<Notifications> {
                 children: [
                   ListTile(
                       title: Text('Update Profile',
-                        style: TextStyle(fontWeight: FontWeight.bold),
+                        style: TextStyle(fontWeight: FontWeight.w900,fontFamily: 'Poppins'),
                       ),
                       subtitle:Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text('Please update your profile'),
-                          TextButton(onPressed: (){},
-                              child: Text('Go to Profile')
+                          Text('Please update your profile',style: TextStyle(fontFamily: 'Poppins')),
+                          InkWell(
+                            onTap: (){},
+                            child: Text('go to Profile',
+                              style: TextStyle(fontWeight: FontWeight.w900,color: Color(0xFF0669BD),fontFamily: 'Poppins'),),
                           )
                         ],
                       ),
@@ -54,23 +57,34 @@ class _NotificationsState extends State<Notifications> {
                         width: 50,
                         height: 50,
                         decoration: BoxDecoration(
-                          color: Colors.blue,
+                          color: Color(0xFF0669BD),
                           shape: BoxShape.circle,
                         ),
                         child: Center(
                           child: Text('T',
-                            style: TextStyle(color: Colors.white,fontWeight: FontWeight.w900,fontSize: 28),),
+                            style: TextStyle(color: Colors.white,fontWeight: FontWeight.w900,fontSize: 28,fontFamily: 'Poppins'),),
                         ),
                       )
                   ),
                   Divider(
                     indent: 15,
                     endIndent: 15,
+                    thickness: 2,
                   ),
                   ListTile(
-                      title: Text('New Message',
-                        style: TextStyle(fontWeight: FontWeight.bold),),
-                      subtitle: Text('You have a new message'),
+                      title: Text('Update Menu',
+                        style: TextStyle(fontWeight: FontWeight.w900,fontFamily: 'Poppins'),),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Check the latest menu, maybe it suits you',style: TextStyle(fontFamily: 'Poppins'),),
+                          InkWell(
+                            onTap: (){},
+                            child: Text('See details',
+                              style: TextStyle(fontWeight: FontWeight.w900,color: Color(0xFF0669BD),fontFamily: 'Poppins'),),
+                          )
+                        ],
+                      ),
                       trailing: ClipRRect(
                         borderRadius: BorderRadius.circular(14),
                         child: Image.network('https://plus.unsplash.com/premium_photo-1671013032391-e6cff46babe5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80',
@@ -83,11 +97,23 @@ class _NotificationsState extends State<Notifications> {
                   Divider(
                     indent: 15,
                     endIndent: 15,
+                    thickness: 2,
                   ),
                   ListTile(
-                    title: Text('Reminder',
-                      style: TextStyle(fontWeight: FontWeight.bold),),
-                    subtitle: Text('Don\'t forget to attend the meeting'),
+                    title: Text('Order Status',
+                      style: TextStyle(fontWeight: FontWeight.w900,fontFamily: 'Poppins'),),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Your order is being processed',
+                        style: TextStyle(fontFamily: 'Poppins'),),
+                        InkWell(
+                          onTap: (){},
+                          child: Text('See details',
+                            style: TextStyle(fontWeight: FontWeight.w900,color: Color(0xFF0669BD),fontFamily: 'Poppins'),),
+                        )
+                      ],
+                    ),
                     trailing: ClipRRect(
                       borderRadius: BorderRadius.circular(14),
                       child: Image.network('https://plus.unsplash.com/premium_photo-1671013032391-e6cff46babe5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80',
@@ -100,6 +126,7 @@ class _NotificationsState extends State<Notifications> {
                   Divider(
                     indent: 15,
                     endIndent: 15,
+                    thickness: 2,
                   ),
                 ],
               ),
