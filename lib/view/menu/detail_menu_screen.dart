29a00837122d39a4b181bbcd5b6b59e79c6309a0
anamilatul <anamilatulaulia@gmail.com>
2023-06-13 21:52:53 +0700

    update detail menu

--- lib/view/menu/detail_menu_screen.dart
+++ lib/view/menu/detail_menu_screen.dart
@@ -21,198 +21,200 @@ class _DetailMenuScreenState extends State<DetailMenuScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Image.network(
-            widget.menuModel.images,
-            width: MediaQuery.of(context).size.width,
-            height: 370,
-            fit: BoxFit.cover,
-          ),
-          Column(
-            children: [
-              AppBar(
-                backgroundColor: Colors.transparent,
-                elevation: 0,
-                leading: IconButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  icon: Container(
-                    padding: const EdgeInsets.all(4),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(100),
-                      color: const Color.fromRGBO(0, 0, 0, 0.6),
-                    ),
-                    child: const Icon(
-                      Icons.arrow_back,
-                      color: ColorTheme.light1,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Image.network(
+              widget.menuModel.images,
+              width: MediaQuery.of(context).size.width,
+              height: 340,
+              fit: BoxFit.cover,
+            ),
+            Column(
+              children: [
+                AppBar(
+                  backgroundColor: Colors.transparent,
+                  elevation: 0,
+                  leading: IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: Container(
+                      padding: const EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        color: const Color.fromRGBO(0, 0, 0, 0.6),
+                      ),
+                      child: const Icon(
+                        Icons.arrow_back,
+                        color: ColorTheme.light1,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Expanded(
-                child: ListView(
-                  children: [
-                    const SizedBox(
-                      height: 220,
-                    ),
-                    Container(
-                      padding: const EdgeInsets.only(top: 10),
-                      margin: const EdgeInsets.symmetric(horizontal: 20),
-                      width: MediaQuery.of(context).size.width,
-                      decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.vertical(
-                          top: Radius.circular(5),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      const SizedBox(
+                        height: 290,
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 10),
+                        margin: const EdgeInsets.symmetric(horizontal: 20),
+                        width: MediaQuery.of(context).size.width,
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.vertical(
+                            top: Radius.circular(5),
+                          ),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              widget.menuModel.name,
+                              style: ThemeText.bodyT18,
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      widget.menuModel.city,
+                                      style: ThemeText.bodyR14,
+                                    ),
+                                    Text(
+                                      '${widget.menuModel.calorie} kkal',
+                                      style: ThemeText.bodyR14,
+                                    ),
+                                  ],
+                                ),
+                                Text(
+                                  priceFormat.format(int.parse(
+                                    widget.menuModel.price,
+                                  )),
+                                  style: ThemeText.bodyB20,
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            const Divider(
+                              thickness: 1,
+                              color: ColorTheme.primaryBlue60,
+                            ),
+                            Text(
+                              'Description',
+                              style: ThemeText.bodyT18,
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              widget.menuModel.description,
+                              style: ThemeText.bodyR14,
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            const Divider(
+                              thickness: 1,
+                              color: ColorTheme.primaryBlue60,
+                            ),
+                            Text(
+                              'Ingredients',
+                              style: ThemeText.bodyT18,
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              widget.menuModel.ingredients,
+                              style: ThemeText.bodyR14,
+                            ),
+                          ],
                         ),
                       ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            widget.menuModel.name,
-                            style: ThemeText.bodyT18,
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    widget.menuModel.city,
-                                    style: ThemeText.bodyR14,
-                                  ),
-                                  Text(
-                                    '${widget.menuModel.calorie} kkal',
-                                    style: ThemeText.bodyR14,
-                                  ),
-                                ],
-                              ),
-                              Text(
-                                priceFormat.format(int.parse(
-                                  widget.menuModel.price,
-                                )),
-                                style: ThemeText.bodyB20,
-                              ),
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 5,
-                          ),
-                          const Divider(
-                            thickness: 1,
-                            color: ColorTheme.primaryBlue60,
-                          ),
-                          Text(
-                            'Description',
-                            style: ThemeText.bodyT18,
-                          ),
-                          const SizedBox(
-                            height: 5,
-                          ),
-                          Text(
-                            widget.menuModel.description,
-                            style: ThemeText.bodyR14,
-                          ),
-                          const SizedBox(
-                            height: 5,
-                          ),
-                          const Divider(
-                            thickness: 1,
-                            color: ColorTheme.primaryBlue60,
-                          ),
-                          Text(
-                            'Ingredients',
-                            style: ThemeText.bodyT18,
-                          ),
-                          const SizedBox(
-                            height: 5,
-                          ),
-                          Text(
-                            widget.menuModel.ingredients,
-                            style: ThemeText.bodyR14,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
-                decoration: const BoxDecoration(
-                  border: Border.symmetric(
-                    horizontal: BorderSide(color: ColorTheme.dark5),
+                    ],
                   ),
                 ),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: ElevatedButton(
-                        onPressed: () {
-                          //logic add to cart
-                          Navigator.push(
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                  decoration: const BoxDecoration(
+                    border: Border.symmetric(
+                      horizontal: BorderSide(color: ColorTheme.dark5),
+                    ),
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            //logic add to cart
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const CartScreen()));
+                            CustomSnackbar.showSnackbar(
                               context,
-                              MaterialPageRoute(
-                                  builder: (context) => const CartScreen()));
-                          CustomSnackbar.showSnackbar(
-                            context,
-                            '"${widget.menuModel.name}" put in basket.',
-                            actionText: 'Close',
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                          );
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: ColorTheme.light1,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(50),
-                            side:
-                                const BorderSide(color: ColorTheme.primaryBlue),
+                              '"${widget.menuModel.name}" put in basket.',
+                              actionText: 'Close',
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: ColorTheme.light1,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50),
+                              side: const BorderSide(
+                                  color: ColorTheme.primaryBlue),
+                            ),
+                          ),
+                          child: Text(
+                            'Add To Cart',
+                            style: ThemeText.bodyR14B,
                           ),
                         ),
-                        child: Text(
-                          'Add To Cart',
-                          style: ThemeText.bodyR14B,
-                        ),
                       ),
-                    ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    Expanded(
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      OrderScreen(widget.menuModel)));
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: ColorTheme.primaryBlue,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(50),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        OrderScreen(widget.menuModel)));
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: ColorTheme.primaryBlue,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                          ),
+                          child: Text(
+                            'Order Now',
+                            style: ThemeText.bodyB145W,
                           ),
                         ),
-                        child: Text(
-                          'Order Now',
-                          style: ThemeText.bodyB145W,
-                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
